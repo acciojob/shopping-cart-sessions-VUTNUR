14f3218ce043861afca9a4a588871934c79d4c1e
Vutnur Nethaji Goud <130832771+VUTNUR@users.noreply.github.com>
2023-07-03 16:17:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -11,6 +11,10 @@ const products = [
 
 // DOM elements
 const productList = document.getElementById("product-list");
+const cartList=document.getElementById("cart-list");
+const clearCartBtn=document.getElementById("clear-cart-btn");
+
+let cartItems= JSON.parse(sessionStorage.getItem("CItems"))||[];
 
 // Render product list
 function renderProducts() {
@@ -22,16 +26,58 @@ function renderProducts() {
 }
 
 // Render cart list
-function renderCart() {}
+function renderCart() {
+  // console.log(cartItems)
+  cartList.innerHTML="";
+  cartItems.forEach((item)=>{
+    const li = document.createElement("li");
+       li.innerHTML = `${item.name} - $${item.price} <button class="remove-from-cart-btn" data-id="${item.id}">Remove from Cart</button>`;
+       cartList.appendChild(li);
+ })
+}
 
 // Add item to cart
-function addToCart(productId) {}
+function addToCart(productId) {
+  //  console.log(productId);
+  for(let i=0;i<products.length;i++){
+    // console.log(products[i].id)
+    if(products[i].id==productId){
+      // console.log(products[i])
+      cartItems.push(products[i]);
+    }
+  }
+  sessionStorage.setItem("CItems",JSON.stringify(cartItems));
+  renderCart();
+}
 
 // Remove item from cart
-function removeFromCart(productId) {}
+function removeFromCart(productId) {
+   cartItems=cartItems.filter((element)=>{
+    return element.id != productId;
+  })
+  console.log(cartItems)
+  renderCart();
+}
 
 // Clear cart
 function clearCart() {}
+
+productList.addEventListener("click",(event)=>{
+	//  console.log(event.target);
+  const id= event.target.getAttribute("data-id");
+  addToCart(id);
+})
+
+cartList.addEventListener("click",(event)=>{
+   const id= event.target.getAttribute("data-id");
+   removeFromCart(id);
+})
+
+clearCartBtn.addEventListener("click", (event)=>{
+  cartItems=[];
+  sessionStorage.removeItem("CItems");
+  renderCart();
+})
 
 // Initial render
 renderProducts();
